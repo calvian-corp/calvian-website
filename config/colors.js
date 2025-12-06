5debf09b7bc2c5f305f620df0dda383bf2010348
Justin Morgan <2justinmorgan@gmail.com>
2025-12-06 15:39:14 -0800

    Brighten red accent color

--- config/colors.js
+++ config/colors.js
@@ -2,9 +2,9 @@
 export default {
 	primary: '#009000',
 	secondary: '#000000',
-	accent: '#c23b4e',
+	accent: '#cd0923ff',
 	'gray-base': '#6F787D', // Slate Gray
-	'hover-accent': '#a80707', // Darker shade for consistency
+	'hover-accent': '#9d0707ff', // Darker shade for consistency
 	'primary-hover': '#009f00ff',
 };
 
